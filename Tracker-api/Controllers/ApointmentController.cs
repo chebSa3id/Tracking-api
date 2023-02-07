1e1707b8832b8a9350be35c8a601235d89a167ef
chebSa3id <mohamedsaed8@live.com>
2023-02-07 10:01:52 +0200

    finishing_it

--- Tracker-api/Controllers/ApointmentController.cs
+++ Tracker-api/Controllers/ApointmentController.cs
@@ -13,12 +13,60 @@ namespace Tracker_api.Controllers
         private readonly ApointmentDbContext _context;
         public ApointmentController(ApointmentDbContext context) => _context = context;
         [HttpGet]
-        public async Task<IEnumerable<Appointment>> Get() {
+        public async Task<IEnumerable<Appointment>> Get()
+        {
             return await _context.Appointments.ToListAsync();
         }
-        [HttpPost]
-        public async Task<IEnumerable<Appointment>> Post()
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(Appointment), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetById(int id)
         {
+            var appointment = await _context.Appointments.FindAsync(id);
+            return appointment == null ? NotFound() : Ok(appointment);
+        }
+        [HttpGet("search/{date}")]
+        [ProducesResponseType(typeof(Appointment), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetByTitle(string date)
+        {
+            var appointment = await _context.Appointments.SingleOrDefaultAsync(c => c.Date == date);
+            return appointment == null ? NotFound() : Ok(appointment);
+        }
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public async Task<IActionResult> Create(Appointment appointment)
+        {
+            await _context.Appointments.AddAsync(appointment);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetById), new { id = appointment.Id }, appointment);
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Update(int id, Appointment appointment)
+        {
+            if (id != appointment.Id) return BadRequest();
+
+            _context.Entry(appointment).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var appointmentToDelete = await _context.Appointments.FindAsync(id);
+            if (appointmentToDelete == null) return NotFound();
+
+            _context.Appointments.Remove(appointmentToDelete);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
         }
     }
 }
--- Tracker-api/Program.cs
+++ Tracker-api/Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<ApointmentDbContext>(x => x.UseSqlServer());
+builder.Services.AddDbContext<ApointmentDbContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer")));
 
 var app = builder.Build();
 
